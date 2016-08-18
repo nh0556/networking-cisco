7b1fb2128e7199966c8a1ea93e7c639ea76a7ef1
nh0556 <NelsonHuang@gmail.com>
2016-08-18 14:06:16 -0400

    DE2286 - Corrected basic debug client to use correct topic for requesting debug state from agent

--- tools/sh_tech_cfg_agent.py
+++ tools/sh_tech_cfg_agent.py
@@ -1,19 +1,15 @@
-from oslo_config import cfg                                                                                                                                                                                                                     
-import oslo_messaging                                                                                                                                                                                                                           
-import pprint                                                                                                                                                                                                                                   
-import oslo_messaging                                                                                                                                                                                                                           
+from oslo_config import cfg
+import oslo_messaging
+import pprint
 
-
-amqp_control_exchange = 'neutron'                                                                                                                                                                                                                                                
-transport_url = 'rabbit://guest:guest@10.1.185.72:5672/'                                                                                                                                                                                        
-# This call to get_transport will bootstrap cfg.CONF                                                                                                                                                                                            
+host = 'mitaka'
+amqp_control_exchange = 'neutron'
+#transport_url = 'rabbit://guest:guest@10.1.185.72:5672/'
+#transport_url = 'rabbit://guest:guest@172.29.74.138:5672/'
+transport_url = 'rabbit://stackrabbit:simple@172.29.74.138:5672/'
+# This call to get_transport will bootstrap cfg.CONF
 transport = oslo_messaging.get_transport(cfg.CONF)
-                                                                                                                                                                                                                                                
-# dump configuration
-# print ("*** initial configuration after get_transport invoked")
-# res = [ {k:v} for k, v in cfg.CONF.iteritems()]
-# pprint.pprint(res)
-                                                                                                                                                                                                                                                
+
 # override config settings
 cfg.CONF.set_override('rabbit_host', 'localhost', 'oslo_messaging_rabbit')
 cfg.CONF.set_override('rabbit_port', 5672, 'oslo_messaging_rabbit')
@@ -21,26 +17,25 @@ cfg.CONF.set_override('rabbit_userid', 'guest', 'oslo_messaging_rabbit')
 cfg.CONF.set_override('rabbit_password', 'guest', 'oslo_messaging_rabbit')
 cfg.CONF.set_override('rabbit_virtual_host', '/', 'oslo_messaging_rabbit')
 cfg.CONF.set_override('control_exchange', amqp_control_exchange)
-                                                                                                                                                                                                                                                
+
 print ("*** rpc_client started")
-                                                                                                                                                                                                                                                
-# dump configuration
 print ("*** oslo_cfg configuration")
-res = [ {k:v} for k, v in cfg.CONF.iteritems()]
+res = [{k: v} for k, v in cfg.CONF.iteritems()]
 pprint.pprint(res)
-                                                                                                                                                                                                                                                
-#
+
+
 transport = oslo_messaging.get_transport(cfg.CONF, transport_url)
 
-target = oslo_messaging.Target(topic='cisco_cfg_agent', version='1.0')
-                                                                                                                                                                                                                                                
+target_topic = "%s.%s" % ('cisco_cfg_agent_l3_routing', host)
+
+target = oslo_messaging.Target(topic=target_topic, version='1.1')
+
 client = oslo_messaging.RPCClient(transport, target)
-                                                                                                                                                                                                                                                
+
 context = {}
 client.cast(context, 'cfg_agent_debug')
 print ("*** rpc_client cast invoked")
-                                                                                                                                                                                                                                                
+
 resp = client.call(context, 'cfg_agent_debug')
-print("*** rpc call response = %s" % pprint.pformat(resp))
-                                                                                                                                                                                                                                                
-print ("*** rpc_client finished")
+print("*** rpc call response = %s" % resp)
+
